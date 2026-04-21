0fc5d00e57f9777b747e01d4f7c8c1689bb56bbd
SebastianAnea <sebastiananea@gmail.com>
2026-04-20 23:22:04 -0300

    Agregar rutas para la navegación de héroes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HeroListComponent } from './heroes/hero-list/hero-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'heroes', pathMatch: 'full' },
+    { path: 'heroes', component: HeroListComponent },
+    { path: '**', redirectTo: 'heroes' },
+];
