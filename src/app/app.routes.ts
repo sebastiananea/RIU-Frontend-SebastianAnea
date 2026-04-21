c12e17187b485f8e73c312982c2a1a6682ca8fe6
SebastianAnea <sebastiananea@gmail.com>
2026-04-21 13:29:27 -0300

    feat: routes heroform and inputBinding from url

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { HeroListComponent } from './heroes/hero-list/hero-list.component';
+import { HeroFormComponent } from './heroes/hero-form/hero-form.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'heroes', pathMatch: 'full' },
-    { path: 'heroes', component: HeroListComponent },
-    { path: '**', redirectTo: 'heroes' },
+  { path: '', redirectTo: 'heroes', pathMatch: 'full' },
+  { path: 'heroes', component: HeroListComponent },
+  { path: 'heroes/new', component: HeroFormComponent },
+  { path: 'heroes/edit/:id', component: HeroFormComponent },
+  { path: '**', redirectTo: 'heroes' },
 ];
